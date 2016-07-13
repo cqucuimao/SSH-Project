1b2d5507e74667e22973c4ed07e961cde672d138
cuimao <cuimao@138713d6-2722-b04c-9809-0d1aa3304408>
2016-07-13 09:23:30 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@194 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/service/car/impl/CarExamineServiceImpl.java
+++ src/main/java/com/yuqincar/service/car/impl/CarExamineServiceImpl.java
@@ -1,5 +1,7 @@
 package com.yuqincar.service.car.impl;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +15,7 @@ import com.yuqincar.dao.car.CarDao;
 import com.yuqincar.dao.car.CarExamineDao;
 import com.yuqincar.domain.car.Car;
 import com.yuqincar.domain.car.CarExamine;
+import com.yuqincar.domain.car.PlateTypeEnum;
 import com.yuqincar.domain.common.PageBean;
 import com.yuqincar.domain.privilege.User;
 import com.yuqincar.service.car.CarExamineService;
@@ -80,6 +83,70 @@ public class CarExamineServiceImpl implements CarExamineService {
 
 	public List<Car> getAllNeedExamineCars() {
 		return carDao.getAllNeedExamineCars();
+	}
+
+	public Date getNextExamineDate(Car car, Date recentExamineDate) {
+		Date nextExamineDate = null;
+		Date registDate = car.getRegistDate();
+		List<Date> dates = new ArrayList<Date>();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(registDate);
+		//蓝牌车且座位数小于7(假设报废期限30年)
+		if(car.getPlateType() == PlateTypeEnum.BLUE && car.getSeatNumber() < 7){
+			//前六年，两年一审
+			for(int i=0;i<6;i++){					
+				calendar.add(Calendar.YEAR, +2);
+				dates.add(calendar.getTime());
+				calendar.setTime(calendar.getTime());
+				
+			}
+			//六年以上十年以下，一年一审
+			calendar.setTime(dates.get(5));
+			for(int i=6;i<10;i++){
+				calendar.add(Calendar.YEAR, +1);
+				dates.add(calendar.getTime());
+				calendar.setTime(calendar.getTime());
+			}
+			//十年以上，一年两审
+			calendar.setTime(dates.get(9));
+			for(int i=10;i<50;i++){
+				calendar.add(Calendar.MONTH, +6);
+				dates.add(calendar.getTime());
+				calendar.setTime(calendar.getTime());
+			}
+			for(int i=0;i<dates.size();i++){
+				if(recentExamineDate.getTime()<dates.get(i).getTime()){
+					nextExamineDate = dates.get(i);
+					break;
+				}
+				
+			}
+		}
+		//黄牌车，或者座位数大于7(假设报废期限30年)
+		if(car.getPlateType() == PlateTypeEnum.YELLOW || car.getSeatNumber() >= 7){
+			//前十年，一年一审
+			for(int i=0;i<10;i++){					
+				calendar.add(Calendar.YEAR, +1);
+				dates.add(calendar.getTime());
+				calendar.setTime(calendar.getTime());
+				
+			}
+			//十年以上，一年两审
+			calendar.setTime(dates.get(9));
+			for(int i=10;i<30;i++){
+				calendar.add(Calendar.MONTH, +6);
+				dates.add(calendar.getTime());
+				calendar.setTime(calendar.getTime());
+			}
+			for(int i=0;i<dates.size();i++){
+				if(recentExamineDate.getTime()<dates.get(i).getTime()){
+					nextExamineDate = dates.get(i);
+					break;
+				}
+				
+			}
+		}
+		return nextExamineDate;
 	}
 
 }

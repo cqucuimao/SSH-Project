1b65397a9484e62aec14b7117e0418a0f9014e56
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-07-24 05:08:52 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@285 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20160721zys.sql
@@ -0,0 +1,16 @@
+insert into role(id,name) values(18,'技术保障科人员')
+insert into role_privilege(roles_id,privileges_id)
+values (18,228)
+,(18,231)
+,(18,172)
+, (18,173)
+, (18,196)
+, (18,199)
+, (18,201)
+, (18,206)
+, (18,209)
+, (18,214)
+, (18,221)
+
+
+

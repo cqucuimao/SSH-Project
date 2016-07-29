bee7a1fe6b7f37f9f5251b79efad00d449a1ade6
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-07-29 02:21:31 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@301 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20160729zys.sql
@@ -0,0 +1,4 @@
+insert into privilege(id,name,url,parent_id) values(291,'导出加油信息月报表','/carRefuel_outPutOil_time',171);
+insert into privilege(id,name,url,parent_id) values(292,'加油信息下载','/carRefuel_OilReport',171);
+
+

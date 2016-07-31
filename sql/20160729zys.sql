9359d972d49d4dc3468428d9ed8f9fb5090bc19c
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-07-31 01:15:54 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@306 138713d6-2722-b04c-9809-0d1aa3304408

--- sql/20160729zys.sql
+++ sql/20160729zys.sql
@@ -1,4 +1,9 @@
 insert into privilege(id,name,url,parent_id) values(291,'导出加油信息月报表','/carRefuel_outPutOil_time',171);
 insert into privilege(id,name,url,parent_id) values(292,'加油信息下载','/carRefuel_OilReport',171);
+alter table carwash drop doEngineClean;
+alter table carwash drop doInnerClean;
+alter table carwash drop doPolishing;
+insert into privilege(id,name,url,parent_id) values(311,'导入洗车信息','/carWash_excel',213);
+insert into privilege(id,name,url,parent_id) values(312,'导入洗车信息','/carWash_importExcelFile',213);
 
 

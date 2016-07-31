cb9aa6575b4564b5e506112fc0e2a7aebdf7704c
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-07-31 01:24:46 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@308 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/action/car/CarWashAction.java
+++ src/main/java/com/yuqincar/action/car/CarWashAction.java
@@ -1,5 +1,10 @@
 package com.yuqincar.action.car;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,6 +19,7 @@ import com.opensymphony.xwork2.ModelDriven;
 import com.yuqincar.action.common.BaseAction;
 import com.yuqincar.dao.car.impl.CarWashShopDaoImpl;
 import com.yuqincar.domain.car.Car;
+import com.yuqincar.domain.car.CarCare;
 import com.yuqincar.domain.car.CarWash;
 import com.yuqincar.domain.car.CarWashShop;
 import com.yuqincar.domain.car.Material;
@@ -24,6 +30,7 @@ import com.yuqincar.service.car.CarService;
 import com.yuqincar.service.car.CarWashService;
 import com.yuqincar.service.privilege.UserService;
 import com.yuqincar.utils.DateUtils;
+import com.yuqincar.utils.ExcelUtil;
 import com.yuqincar.utils.QueryHelper;
 @Controller
 @Scope("prototype")
@@ -39,10 +46,23 @@ public class CarWashAction extends BaseAction implements ModelDriven<CarWash> {
 	
 	@Autowired
 	private UserService userService;
+    private File upload;
+	
+	private String uploadFileName;
+	
+	private String uploadContentType;
+	
+	private String failReason;
+	
+	private int result;
 	
 	private Date beginDate;
 	
 	private Date endDate;
+	
+	private String carShop;
+	
+	private CarWashShop carWashShop;
 
 	//头部快速查询
 	public String queryForm(){
@@ -84,6 +104,115 @@ public class CarWashAction extends BaseAction implements ModelDriven<CarWash> {
        carWashService.deleteCarWash(model.getId());
 	   return freshList();
 	}
+   
+   public String excel() throws Exception {
+		return "excel";
+	}
+   
+   public String importExcelFile() throws Exception{
+		InputStream is = new FileInputStream(upload);
+		ExcelUtil eu = new ExcelUtil();
+		List<List<String>> excelLines = eu.getLinesFromExcel(is, 1, 1, 9, 0);
+		List<CarWash> carWash = new ArrayList<CarWash>();
+		List<CarWashShop>  shoplists=new ArrayList<CarWashShop>();
+		boolean flag=false;
+		shoplists=carWashService.getAllCarWashShop();
+		for(int i=1;i<excelLines.size();i++){
+					
+		        try {       
+		        	        
+		        	        result=i;
+							CarWash cw = new CarWash();
+							
+							//车辆
+							//System.out.println("车牌号="+excelLines.get(i).get(0));
+							Car car = carService.getCarByPlateNumber(excelLines.get(i).get(0));
+							if(car == null){
+								failReason = "未知车辆";
+								ActionContext.getContext().getValueStack().push(failReason);
+								ActionContext.getContext().getValueStack().push(result);
+								return "false";
+							}else{
+								cw.setCar(car);
+							}
+							
+							//司机
+							//System.out.println("司机="+excelLines.get(i).get(1));
+							User driver = userService.getByLoginName(excelLines.get(i).get(1));
+							System.out.println("driver= :"+driver);
+							if(driver == null){
+								failReason = "未知司机";
+								ActionContext.getContext().getValueStack().push(failReason);
+								ActionContext.getContext().getValueStack().push(result);
+								return "false";
+							}else{
+								cw.setDriver(driver);				
+							}
+							
+							//洗车时间
+							//System.out.println("洗车时间="+excelLines.get(i).get(2));
+							SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");  
+						    Date date;				
+							date = sdf.parse(excelLines.get(i).get(2));
+							cw.setDate(date);
+							
+							//洗车点管理
+							//System.out.println("洗车点="+excelLines.get(i).get(3));
+							
+							 carShop = excelLines.get(i).get(3);
+							for(int j=0;j<shoplists.size();j++)
+							{
+								carWashShop=shoplists.get(j);
+								//System.out.println(shoplists.get(j));
+								if(carShop.equals(carWashShop.getName()))
+								{
+									flag=true;
+									break;
+								}
+							}
+							if(flag){
+								cw.setShop(carWashShop);
+							}
+							else{
+								failReason = "未知洗车点";
+								ActionContext.getContext().getValueStack().push(failReason);
+								ActionContext.getContext().getValueStack().push(result);
+								return "false";
+							}
+							
+							flag=false;
+							
+							//金额
+							//System.out.println("金额="+excelLines.get(i).get(4));
+							BigDecimal bd = new BigDecimal(excelLines.get(i).get(4));
+							cw.setMoney(bd);
+							
+							
+							BigDecimal inner = new BigDecimal(excelLines.get(i).get(5));
+							cw.setInnerCleanMoney(inner);
+							BigDecimal po = new BigDecimal(excelLines.get(i).get(6));
+							cw.setPolishingMoney(po);
+							BigDecimal en = new BigDecimal(excelLines.get(i).get(7));
+							cw.setEngineCleanMoney(en);
+							BigDecimal cu = new BigDecimal(excelLines.get(i).get(8));
+							cw.setCushionCleanMoney(cu);
+							
+							carWash.add(cw);
+							
+						} catch (Exception e) {
+							failReason = "不明原因";
+							ActionContext.getContext().getValueStack().push(failReason);
+							ActionContext.getContext().getValueStack().push(result);
+							return "false";
+						}
+				}
+			
+		result = excelLines.size() - 1;
+		carWashService.importExcelFile(carWash);
+		ActionContext.getContext().getValueStack().push(result);
+		return "success";	
+	}
+   
    public String saveUI(){
 	   
 	   ActionContext.getContext().put("carWashShopList", carWashService.getAllCarWashShop());
@@ -120,9 +249,10 @@ public class CarWashAction extends BaseAction implements ModelDriven<CarWash> {
 		carWash.setShop(carWashShop);
 		carWash.setMoney(model.getMoney());
 		carWash.setDate(model.getDate());
-		carWash.setDoPolishing(model.isDoPolishing());
-		carWash.setDoEngineClean(model.isDoEngineClean());
-		carWash.setDoInnerClean(model.isDoInnerClean());
+		carWash.setInnerCleanMoney(model.getInnerCleanMoney());
+		carWash.setPolishingMoney(model.getPolishingMoney());
+		carWash.setEngineCleanMoney(model.getEngineCleanMoney());
+		carWash.setCushionCleanMoney(model.getCushionCleanMoney());
 		carWashService.updateCarWash(carWash);
 		ActionContext.getContext().getValueStack().push(new CarWash());
 		return freshList();
@@ -144,6 +274,36 @@ public class CarWashAction extends BaseAction implements ModelDriven<CarWash> {
 	}
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
+	}
+	public File getUpload() {
+		return upload;
+	}
+	public void setUpload(File upload) {
+		this.upload = upload;
+	}
+	public String getUploadFileName() {
+		return uploadFileName;
+	}
+	public void setUploadFileName(String uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+	public String getUploadContentType() {
+		return uploadContentType;
+	}
+	public void setUploadContentType(String uploadContentType) {
+		this.uploadContentType = uploadContentType;
+	}
+	public String getFailReason() {
+		return failReason;
+	}
+	public void setFailReason(String failReason) {
+		this.failReason = failReason;
+	}
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
 	}
 
 }	

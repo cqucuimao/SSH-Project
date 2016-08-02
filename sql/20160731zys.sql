dff6c4ef9ff7b3b6c71b73811cbec51693ddfe7b
chenhengxin <chenhengxin@138713d6-2722-b04c-9809-0d1aa3304408>
2016-08-02 02:44:58 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@326 138713d6-2722-b04c-9809-0d1aa3304408

--- sql/20160731zys.sql
+++ sql/20160731zys.sql
@@ -1,2 +1,2 @@
-insert into privilege(id,name,url,parent_id) values(313,'权限添加','/user_addUI',11);
-insert into privilege(id,name,url,parent_id) values(314,'详细信息','/car_carDetail',107);
+insert into privilege(id,name,url,parent_id) values(313,'详细信息','/car_carDetail',107);
+insert into role_privilege(roles_id,privileges_id) values (18,313);

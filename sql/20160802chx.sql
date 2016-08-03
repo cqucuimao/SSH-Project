b9aff69767f9f5033fbae2907928598f09f6e8f8
chenhengxin <chenhengxin@138713d6-2722-b04c-9809-0d1aa3304408>
2016-08-03 03:01:44 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@328 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20160802chx.sql
@@ -0,0 +1,5 @@
+insert into privilege(id,name,url,parent_id) values(314,'','/user_editUI',11);
+insert into privilege(id,name,url,parent_id) values(315,'','/user_editUI',11);
+insert into privilege(id,name,url,parent_id) values(316,'','/department_editUI',7);
+insert into privilege(id,name,url,parent_id) values(317,'','/department_addUI',7);
+insert into privilege(id,name,url,parent_id) values(318,'','/role_addUI',2);

4892e79e98772d38a2f3244a8787e83cd138812d
cuimao <cuimao@138713d6-2722-b04c-9809-0d1aa3304408>
2016-08-13 11:12:22 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@391 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20160813cm.sql
@@ -0,0 +1,4 @@
+insert into privilege(id,name,url,parent_id) values(320,'车辆详细查询页面','/car_queryUI',107);
+insert into privilege(id,name,url,parent_id) values(321,'车辆详细查询','/car_moreQuery',107);
+insert into privilege(id,name,url,parent_id) values(322,'订单详细查询页面','/order_queryUI',39);
+insert into privilege(id,name,url,parent_id) values(323,'订单详细查询','/order_moreQuery',39);

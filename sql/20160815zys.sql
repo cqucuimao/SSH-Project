97268c940bca11c6f7944ae2594a1bedfb038f4b
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-08-15 11:40:59 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@410 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20160815zys.sql
@@ -0,0 +1,3 @@
+insert into privilege(id,name,url,parent_id) values(324,'修改单位财务需求','/customerOrganization_addFinancial',249);
+insert into privilege(id,name,url,parent_id) values(325,'修改单位财务需求','/customerOrganization_editFinancial',249);
+insert into role_privilege(roles_id,privileges_id) values(4,249);

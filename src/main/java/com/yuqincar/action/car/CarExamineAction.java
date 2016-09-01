ea88ea1ad9491479b64251fe503032861dd6773c
cuimao <cuimao@138713d6-2722-b04c-9809-0d1aa3304408>
2016-09-01 04:24:13 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@532 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/action/car/CarExamineAction.java
+++ src/main/java/com/yuqincar/action/car/CarExamineAction.java
@@ -1,5 +1,6 @@
 package com.yuqincar.action.car;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -16,12 +17,16 @@ import com.yuqincar.domain.car.Car;
 import com.yuqincar.domain.car.CarCare;
 import com.yuqincar.domain.car.CarExamine;
 import com.yuqincar.domain.car.CarRepair;
+import com.yuqincar.domain.car.CarStatusEnum;
+import com.yuqincar.domain.car.CarViolation;
+import com.yuqincar.domain.car.TollCharge;
 import com.yuqincar.domain.common.BaseEntity;
 import com.yuqincar.domain.common.PageBean;
 import com.yuqincar.domain.order.Order;
 import com.yuqincar.domain.privilege.User;
 import com.yuqincar.service.car.CarExamineService;
 import com.yuqincar.service.car.CarService;
+import com.yuqincar.service.car.CarViolationService;
 import com.yuqincar.service.order.OrderService;
 import com.yuqincar.service.privilege.UserService;
 import com.yuqincar.utils.DateUtils;
@@ -46,6 +51,9 @@ public class CarExamineAction extends BaseAction implements ModelDriven<CarExami
 	private OrderService orderService;
 	
 	@Autowired
+	private CarViolationService carViolationService;
+	
+	@Autowired
 	private UserService userService;
 	
 	private Date recentExamineDate;
@@ -55,6 +63,20 @@ public class CarExamineAction extends BaseAction implements ModelDriven<CarExami
 	private Date date2;
 	
 	private Long carId;
+	
+	private Date payDate;
+	
+	private BigDecimal tollChargeMoney;
+	
+	private BigDecimal overdueFine;
+	
+	private BigDecimal moneyForCardReplace;
+	
+	private Date nextPayDate;
+	
+	private String actionFlag;
+	
+	private String yearMonth;
 	
 	public String queryForm() throws Exception {
 		QueryHelper helper = new QueryHelper(CarExamine.class, "ce");
@@ -162,6 +184,8 @@ public class CarExamineAction extends BaseAction implements ModelDriven<CarExami
 	}
 	
 	public String addUI() throws Exception {
+		System.out.println("actionFlag="+actionFlag);
+		ActionContext.getContext().put("actionFlag", actionFlag);
 		return "saveUI";
 	}
 	
@@ -178,7 +202,16 @@ public class CarExamineAction extends BaseAction implements ModelDriven<CarExami
 		}
 		
 		model.setAppointment(false);
-		carExamineService.saveCarExamine(model);
+		
+		TollCharge tollCharge = new TollCharge();
+		tollCharge.setCar(model.getCar());
+		tollCharge.setPayDate(payDate);
+		tollCharge.setMoney(tollChargeMoney);
+		tollCharge.setNextPayDate(nextPayDate);
+		tollCharge.setOverdueFine(overdueFine);
+		tollCharge.setMoneyForCardReplace(moneyForCardReplace);
+		
+		carExamineService.saveCarExamine(model,tollCharge);
 		model=null;
 		ActionContext.getContext().getValueStack().push(getModel());
 		return freshList();
@@ -187,7 +220,6 @@ public class CarExamineAction extends BaseAction implements ModelDriven<CarExami
 	public String editUI() throws Exception {
 		// 准备回显的数据
 		CarExamine carExamine = carExamineService.getCarExamineById(model.getId());
-		
 		carExamine.setDate(DateUtils.getYMD(DateUtils.getYMDString(carExamine.getDate())));
 		carExamine.setNextExamineDate(DateUtils.getYMD(DateUtils.getYMDString(carExamine.getNextExamineDate())));
 		
@@ -245,9 +277,67 @@ public class CarExamineAction extends BaseAction implements ModelDriven<CarExami
 	
 	/** 提醒*/
 	public String remind() throws Exception{
-		PageBean<Car> pageBean=carExamineService.getNeedExamineCars(pageNum);
+		QueryHelper helper = new QueryHelper(Car.class, "c");
+		Date now=new Date();
+		Date b = new Date(now.getTime() +  24*60*60*1000 * 15L );
+		helper.addWhereCondition("c.nextExaminateDate < ? and c.status=? and c.borrowed=?", b,CarStatusEnum.NORMAL,false);
+		helper.addOrderByProperty("c.nextExaminateDate", true);	
+		PageBean<Car> pageBean=carExamineService.getNeedExamineCars(pageNum,helper);
+		ActionContext.getContext().getValueStack().push(pageBean);
+		ActionContext.getContext().getSession().put("carHelper1", helper);
+		return "remindList";
+	}
+	
+	//查询提醒
+	public String queryRemind() throws Exception{
+		QueryHelper helper = new QueryHelper(Car.class, "c");
+		Date now=new Date();
+		Date b = new Date(now.getTime() +  24*60*60*1000 * 15L );
+		System.out.println("b="+b);
+		helper.addWhereCondition("c.nextExaminateDate < ? and c.status=? and c.borrowed=?", b,CarStatusEnum.NORMAL,false);	
+		if(yearMonth != null && yearMonth.length()>0){
+			String ymd = yearMonth + "-01";
+			Date d1= DateUtils.getYMD(ymd);
+			Date d2 = DateUtils.getEndDateOfMonth(d1);
+			System.out.println("d1="+d1);
+			System.out.println("d2="+d2);
+			helper.addWhereCondition("(TO_DAYS(c.nextExaminateDate) - TO_DAYS(?)) >=0 and (TO_DAYS(c.nextExaminateDate) - TO_DAYS(?)) <= 0", d1,d2);
+		}		
+		helper.addOrderByProperty("c.nextExaminateDate", true);	
+		PageBean<Car> pageBean=carExamineService.getNeedExamineCars(pageNum,helper);
+		ActionContext.getContext().getValueStack().push(pageBean);
+		ActionContext.getContext().getSession().put("carHelper1", helper);
+		return "remindList";
+	}
+	
+	public String freshRemind(){
+		
+		QueryHelper helper = (QueryHelper)ActionContext.getContext().getSession().get("carHelper1");
+		PageBean<Car> pageBean=carExamineService.getNeedExamineCars(pageNum,helper);
 		ActionContext.getContext().getValueStack().push(pageBean);
 		return "remindList";
+	}
+	
+	public String getProtocolInfo(){
+		String protocolInfo = "";
+		Car car=(Car)ActionContext.getContext().getValueStack().peek();
+		Order order = orderService.getProtocolOrderByCar(car);
+		if(order != null){
+			protocolInfo = order.getCustomerOrganization().getName()+"("+order.getCustomer().getName()+" "+order.getPhone()+")";
+		}
+		return protocolInfo;
+	}
+	
+	public String getCarViolationCount(){
+		System.out.println("++++++++");
+		String carViolationCount = "";
+		Car car=(Car)ActionContext.getContext().getValueStack().peek();
+		List<CarViolation> carViolations = carViolationService.getCarViolationByCar(car);
+		System.out.println("&&&&&&&&&");
+		if(carViolations != null && carViolations.size()>0){
+			carViolationCount = carViolations.size()+"";
+		}
+		return carViolationCount;
 	}
 	
 	/** 详细信息*/
@@ -421,4 +511,61 @@ public class CarExamineAction extends BaseAction implements ModelDriven<CarExami
 	public void setCarId(Long carId) {
 		this.carId = carId;
 	}
+
+	public Date getPayDate() {
+		return payDate;
+	}
+
+	public void setPayDate(Date payDate) {
+		this.payDate = payDate;
+	}
+
+	public BigDecimal getTollChargeMoney() {
+		return tollChargeMoney;
+	}
+
+	public void setTollChargeMoney(BigDecimal tollChargeMoney) {
+		this.tollChargeMoney = tollChargeMoney;
+	}
+
+	public BigDecimal getOverdueFine() {
+		return overdueFine;
+	}
+
+	public void setOverdueFine(BigDecimal overdueFine) {
+		this.overdueFine = overdueFine;
+	}
+
+	public BigDecimal getMoneyForCardReplace() {
+		return moneyForCardReplace;
+	}
+
+	public void setMoneyForCardReplace(BigDecimal moneyForCardReplace) {
+		this.moneyForCardReplace = moneyForCardReplace;
+	}
+
+	public Date getNextPayDate() {
+		return nextPayDate;
+	}
+
+	public void setNextPayDate(Date nextPayDate) {
+		this.nextPayDate = nextPayDate;
+	}
+
+	public String getActionFlag() {
+		return actionFlag;
+	}
+
+	public void setActionFlag(String actionFlag) {
+		this.actionFlag = actionFlag;
+	}
+
+	public String getYearMonth() {
+		return yearMonth;
+	}
+
+	public void setYearMonth(String yearMonth) {
+		this.yearMonth = yearMonth;
+	}
+
 }

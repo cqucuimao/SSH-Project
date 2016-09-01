4de77cc15082fa48d23cbaa116ced5aa37f38356
cuimao <cuimao@138713d6-2722-b04c-9809-0d1aa3304408>
2016-09-01 04:23:42 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@531 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20160831cm.sql
@@ -0,0 +1,2 @@
+insert into privilege(id,name,url,parent_id) values(338,'','/carExamine_queryRemind',181);
+insert into privilege(id,name,url,parent_id) values(339,'','/carExamine_freshRemind',181);

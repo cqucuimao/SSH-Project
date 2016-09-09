5038ce4316a07ac842a0ec7169a17f446b3ad423
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-09-09 05:27:19 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@584 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/tags/CarDetailListTag.java
+++ src/main/java/com/yuqincar/tags/CarDetailListTag.java
@@ -1,26 +1,47 @@
 package com.yuqincar.tags;
 
 import java.io.IOException;
-
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.classic.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import org.apache.struts2.views.jsp.TagUtils;
-
-import com.opensymphony.xwork2.util.ValueStack;
+import com.opensymphony.xwork2.inject.Context;
+import com.yuqincar.dao.car.CarDao;
+import com.yuqincar.dao.car.impl.CarDaoImpl;
 import com.yuqincar.domain.car.Car;
+import com.yuqincar.service.car.CarService;
+import com.yuqincar.service.car.impl.CarServiceImpl;
+import javax.annotation.Resource;
 
 public class CarDetailListTag extends TagSupport {
 	private static final long serialVersionUID = 1L;
 	private String id;
-	private String value;
+	@Resource
+	private CarService carService;
+	//CarDao carDao = new CarDaoImpl();
+	/*WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(ServletActionContext.getServletContext());  
+	CarServiceType carServiceType = (CarServiceType)wac.getBean("carServiceType"); 
+	*/
 	public int doStartTag() throws JspException {
 		StringBuffer options = new StringBuffer();
 		JspWriter out = pageContext.getOut();
 		try {
 			System.out.println("id=: "+id);
-			System.out.println("value=: "+value);
+			WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(ServletActionContext.getServletContext());  
+
+		   CarService carService = (CarService)wac.getBean("carService"); 
+			/*ApplicationContext  ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+			CarService carService = (CarService)ac.getBean("carService");*/
+			System.out.println("carService:= "+carService);
+			String  value=carService.getCarById(Long.parseLong(id)).getPlateNumber();
+			
 			options.append("<a href=\"/Web/car_carDetail.action?id="+id+"\"> "+value+" </a>");
 			out.println(options);
 		} catch (IOException e) {
@@ -31,14 +52,7 @@ public class CarDetailListTag extends TagSupport {
 	}
 
 	public int doEndTag() throws JspException {
-		/*StringBuffer options = new StringBuffer();
-		JspWriter out = pageContext.getOut();
-		try {
-			options.append("</s:a>");
-			out.println(options);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}*/
+		
 		return EVAL_PAGE;
 	}
 
@@ -50,12 +64,4 @@ public class CarDetailListTag extends TagSupport {
 		this.id = id;
 	}
 
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-	
 }

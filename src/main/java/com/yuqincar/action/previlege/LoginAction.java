cf40674c1837005e4d8c9479040539792b9d669a
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-10-20 11:59:17 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@683 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/action/previlege/LoginAction.java
+++ src/main/java/com/yuqincar/action/previlege/LoginAction.java
@@ -1,5 +1,8 @@
 package com.yuqincar.action.previlege;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.inject.Inject;
 import com.yuqincar.action.common.BaseAction;
+import com.yuqincar.domain.common.Company;
 import com.yuqincar.domain.privilege.User;
 import com.yuqincar.service.privilege.PrivilegeService;
 import com.yuqincar.service.privilege.UserService;
@@ -32,6 +36,7 @@ public class LoginAction extends BaseAction{
 	
 	private String validationCode;
 	
+	private Company company;
 	
 	/** 登录页面 */
 	public String loginUI() throws Exception {
@@ -42,15 +47,49 @@ public class LoginAction extends BaseAction{
 	/** 登录 */
 	public String login() throws Exception {
 		User user;
-		if(devMode.equals("false")){
+		if(devMode.equals("false"))
+		{
 			if(!validationCode.equals(ActionContext.getContext().getSession().get("securityCode"))){
 				addFieldError("loginError", "验证码不正确！");
 				return "loginUI";
-			}	
+			}
+			if(company==null)
+			{
+				List<User> Sameusers=new ArrayList<User>();
+				Sameusers=userService.getUsersByLoginName(loginName);
+				System.out.println("***********"+Sameusers.size());
+				if(Sameusers.size()>1)
+				{   //todo 得到有这个用户的公司，然后返回给login界面去选择，并重新登录
+					List<Company> companies=new ArrayList<Company>();
+					for(int i=0;i<Sameusers.size();i++)
+						companies.add(Sameusers.get(i).getCompany());
+					ActionContext.getContext().put("companies",companies);
+					ActionContext.getContext().put("display",true);
+					return "loginUI";
+				}
+			}
 			// 验证用户名与密码，如果正确就返回这个用户，否则返回null
-			user = userService.getByLoginNameAndPassword(loginName, password);
-		}else{
-			user=userService.getByLoginName(loginName);
+			user = userService.getByLoginNameAndPassword(loginName, password,company);
+		}
+		else{
+
+			if(company==null)
+			{
+				List<User> Sameusers=new ArrayList<User>();
+				Sameusers=userService.getUsersByLoginName(loginName);
+				System.out.println("***********"+Sameusers.size());
+				if(Sameusers.size()>1)
+				{   //todo 得到有这个用户的公司，然后返回给login界面去选择，并重新登录
+					List<Company> companies=new ArrayList<Company>();
+					for(int i=0;i<Sameusers.size();i++)
+						companies.add(Sameusers.get(i).getCompany());
+					ActionContext.getContext().put("companies",companies);
+					ActionContext.getContext().put("display",true);
+					return "loginUI";
+				}
+			}
+			user = userService.getByLoginNameAndPassword(loginName, password,company);
+			//user=userService.getByLoginName(loginName);
 		}
 
 		// 如果登录名或密码不正确，就转回到登录页面并提示错误消息
@@ -64,6 +103,8 @@ public class LoginAction extends BaseAction{
 		}
 		
 		ActionContext.getContext().getSession().put("user", user);
+		ActionContext.getContext().getSession().put("company", user.getCompany());
+		ActionContext.getContext().getSession().put("companyLogoName", user.getCompany().getLogoName());
 		ActionContext.getContext().getSession().put("userPrivileges", privilegeService.getUserPrivilegeUrls(user));
 		return "toHome";
 	}
@@ -107,4 +148,13 @@ public class LoginAction extends BaseAction{
 		System.out.println("in isDevMode");
 		return devMode.equals("true");
 	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	
 }

7b9708458d025bb1d3391f142fd44555f779abeb
cuimao <cuimao@138713d6-2722-b04c-9809-0d1aa3304408>
2016-11-23 04:04:09 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@754 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20161118cm.sql
@@ -0,0 +1,6 @@
+insert into privilege(id,name,url,parent_id) values(399,'','/realtime_monitorGroup',68);
+insert into privilege(id,name,url,parent_id) values(400,'','/realtime_monitorGroupAddUI',68);
+insert into privilege(id,name,url,parent_id) values(401,'','/realtime_monitorGroupAdd',68);
+insert into privilege(id,name,url,parent_id) values(402,'','/realtime_monitorGroupEditUI',68);
+insert into privilege(id,name,url,parent_id) values(403,'','/realtime_monitorGroupEdit',68);
+insert into privilege(id,name,url,parent_id) values(404,'','/realtime_delete',68);

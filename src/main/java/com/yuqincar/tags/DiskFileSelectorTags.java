d59f9d9d16035156a61c62438b8a7a369d59c9a2
zengyuansong <zengyuansong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-11-27 11:08:43 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@820 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/tags/DiskFileSelectorTags.java
+++ src/main/java/com/yuqincar/tags/DiskFileSelectorTags.java
@@ -1,6 +1,8 @@
 package com.yuqincar.tags;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.jsp.JspException;
@@ -10,6 +12,7 @@ import javax.servlet.jsp.tagext.TagSupport;
 import org.apache.struts2.views.jsp.TagUtils;
 import org.springframework.expression.spel.ast.OpAnd;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.util.ValueStack;
 import com.yuqincar.domain.common.DiskFile;
 import com.yuqincar.domain.privilege.Department;
@@ -20,11 +23,27 @@ public class DiskFileSelectorTags extends TagSupport {
 	private String name;
 	private String uploadLimit="0";
 	private String mutilfiles="true";
+	private String fileQueue;
 
 	public int doStartTag() throws JspException {
+		int x=(int)(Math.random()*100);
+		fileQueue="fileQueue_"+String.valueOf(x);
+		String sessionName="diskFile_"+name;
+		if(ActionContext.getContext().getSession().get(sessionName)!=null)
+		{
+			 List<File> fileList=(List<File>)ActionContext.getContext().getSession().get(sessionName);
+			 for(File file:fileList)
+			 {
+				 file.delete();
+			 }
+		}
+		ActionContext.getContext().getSession().remove(sessionName);
+		//System.out.println("sessionNmae in selector: "+sessionName);
+		
 		StringBuffer options = new StringBuffer();
 		JspWriter out = pageContext.getOut();
-		System.out.println("mutifiles: "+uploadLimit);
+		//System.out.println("mutifiles: "+uploadLimit);
+		
 		if(uploadLimit.equals("true"))
 		{
 			uploadLimit="1";
@@ -36,48 +55,45 @@ public class DiskFileSelectorTags extends TagSupport {
 			options.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"js/uploadify.css\">");
 			options.append("<script type=\"text/javascript\" src=\"http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js\"></script>");	
 			options.append("<script type=\"text/javascript\" src=\"js/jquery.uploadify.min.js\"></script>");
+			
 			options.append(" <script type=\"text/javascript\">");
 			options.append("$(function(){ ");
-			options.append("$(\"#uploadify\").uploadify({    ");
+			options.append("$(\"#"+name+"\").uploadify({    ");
 			options.append("'debug'     : false, ");
 			options.append("'auto'           : true, ");
-			options.append(" 'swf'            : 'js/uploadify.swf',");
+			options.append(" 'swf'           : 'js/uploadify.swf',");
 			options.append("'uploader'       : 'diskFile.action', ");
-			options.append("'queueID'        : 'fileQueue', ");
+			options.append("'queueID'        : '"+fileQueue+"', ");
 			options.append("'width'          : '75', ");
 			options.append("'height'         : '24',  ");
-			options.append(" 'queueSizeLimit' : '-1', ");
+			options.append(" 'queueSizeLimit': '-1', ");
 			options.append("'uploadLimit'    : "+uploadLimit+", ");
-			options.append(" 'fileTypeDesc'   : '视频文件',");
-			options.append(" 'fileTypeExts'   : '*.*;*.jpg;*.gif',");
+			options.append(" 'fileTypeDesc'  : '视频文件',");
+			options.append(" 'formData'      :{'diskName':'"+name+"'},");
+			options.append(" 'fileTypeExts'  : '*.*;*.jpg;*.gif',");
 			options.append("'multi'          : "+mutilfiles+", ");
 			options.append("'buttonText'     : '文件上传', ");
 			options.append("'fileSizeLimit'  : '500MB', ");
-			options.append(" 'fileObjName'    : 'uploadify',");
-			options.append(" 'method'         : 'post',");
+			options.append(" 'fileObjName'   : 'uploadify',");
+			options.append(" 'method'        : 'post',");
 			options.append(" 'removeCompleted' : false,");
 			options.append(" 'onFallback':function(){ ");
 			options.append(" alert(\"您未安装FLASH控件，无法上传图片！请安装FLASH控件后再试。\"); },   ");
-			
-			options.append("'onUploadSuccess' : function(file, data, response){ "
-					+ "$(\"#first\").val(\"second\");"
-					+ "}, ");
+			options.append("'onUploadSuccess' : function(file, data, response){ },");
 			options.append("'onQueueComplete' : function(){ } ,");
-			options.append("'onUploadStart':function(file){ var first;");
-			options.append("first=$('#first').val();");
-			options.append("$('#uploadify').uploadify('settings','formData',{'first':first});},");
-			
+			options.append("'onUploadStart':function(file){ ");
+			options.append(" },");
 			options.append("'onCancel'           :function(file){ ");
 			options.append(" alert(file.name+\"canceled\");");
 			options.append("	} ");
 			options.append("  });");
 			options.append("}); ");
 			options.append("</script>");
-			options.append(" <input type=\"file\" id=\"uploadify\" name=\"uploadify\">");
-			options.append("<div id=\"fileQueue\"> </div>  ");
-			options.append("<input type=\"hidden\" name=\"first\"  id=\"first\" value=\"first\" />");
-			options.append("<input id=" + name + " multiFiles="+uploadLimit+" name=" + name
-					+ " type=\"hidden\" />");
+			
+			options.append(" <input type=\"file\" id=" + name + " name=\"uploadify\">");
+			options.append("<div id=" + fileQueue + "> </div>  ");
+			options.append("<input  multiFiles="+uploadLimit+" name=" + name
+					+ " value="+ name +" type=\"hidden\" />");
 			out.println(options);
 
 		} catch (IOException e) {
@@ -106,6 +122,14 @@ public class DiskFileSelectorTags extends TagSupport {
 
 	public void setUploadLimit(String uploadLimit) {
 		this.uploadLimit = uploadLimit;
+	}
+
+	public String getMutilfiles() {
+		return mutilfiles;
+	}
+
+	public void setMutilfiles(String mutilfiles) {
+		this.mutilfiles = mutilfiles;
 	}
 
 }

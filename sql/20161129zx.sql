a7636e1462de90641e71835440f05b113b3d8626
zhaoxing <zhaoxing@138713d6-2722-b04c-9809-0d1aa3304408>
2016-11-30 08:30:50 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@851 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20161129zx.sql
@@ -0,0 +1,3 @@
+insert into privilege(id,name,url,parent_id) values(435,'','/businessParameter_reserveCarApplyOrderStandingGarage',395);
+insert into privilege(id,name,url,parent_id) values(436,'','/businessParameter_addReserveCarApplyOrderStandingGarage',395);
+insert into privilege(id,name,url,parent_id) values(437,'','/businessParameter_addCarUI',395);

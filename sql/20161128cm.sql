d3ae0c7a19511084f04d99749dfc0dcc39109938
cuimao <cuimao@138713d6-2722-b04c-9809-0d1aa3304408>
2016-11-30 06:39:37 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@847 138713d6-2722-b04c-9809-0d1aa3304408

---
+++ sql/20161128cm.sql
@@ -0,0 +1,11 @@
+alter table carservicesupertype change title superTitle varchar(255);
+insert into privilege(id,name,url,parent_id) values(435,'','/priceTable_deleteCarServiceSuperType',360);
+insert into privilege(id,name,url,parent_id) values(436,'','/priceTable_deleteCarServiceType',360);
+insert into privilege(id,name,url,parent_id) values(437,'','/priceTable_detail',360);
+insert into privilege(id,name,url,parent_id) values(438,'','/priceTable_addUI',360);
+insert into privilege(id,name,url,parent_id) values(439,'','/priceTable_edit',360);
+insert into privilege(id,name,url,parent_id) values(440,'','/priceTable_add',360);
+insert into privilege(id,name,url,parent_id) values(441,'','/priceTable_editUI',360);
+insert into privilege(id,name,url,parent_id) values(442,'','/priceTable_delete',360);
+insert into privilege(id,name,url,parent_id) values(443,'','/priceTable_getCarServiceTypeOption',360);
+

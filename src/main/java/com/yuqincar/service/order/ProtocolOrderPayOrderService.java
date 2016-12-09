2430d502a02e957abacda7fe3b76cface87ae643
liucong <liucong@138713d6-2722-b04c-9809-0d1aa3304408>
2016-12-09 07:42:06 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@871 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/service/order/ProtocolOrderPayOrderService.java
+++ src/main/java/com/yuqincar/service/order/ProtocolOrderPayOrderService.java
@@ -1,5 +1,8 @@
 package com.yuqincar.service.order;
 
+import java.util.Date;
+import java.util.List;
+
 import com.yuqincar.domain.common.PageBean;
 import com.yuqincar.domain.order.Order;
 import com.yuqincar.domain.order.ProtocolOrderPayOrder;
@@ -17,4 +20,20 @@ public interface ProtocolOrderPayOrderService {
 	public PageBean<ProtocolOrderPayOrder> queryProtocolOrderPayOrder(QueryHelper helper,int pageNum);
 	
 	public boolean canAddProtocolOrderPayOrder(Order order);
+	
+	/**
+	 * 根据对账单名称和相应order的id数组，生成新对账单
+	 * 新对账单的生成包含两个步骤，(1)将相应订单加到新建的对账单中;(2)改变这些加入到对账单中的订单的orderStatement状态
+	 * @param orderStatementName 
+	 * @param ids
+	 */
+	void newOrderStatementByPopoIds(String orderStatementName, Long[] ids);
+
+	/**
+	 * 根据对账单名称和相应order的id数组，将order的id数组对应的对账单加入到相应的对账单名称的对账单中
+	 * 添加对账单包括两个步骤，(1)将相应的订单添加到相应对账单名称对应的对账单中;(2)改变这些加入到对账单中的订单的orderStatement状态
+	 * @param orderStatementName
+	 * @param ids
+	 */
+	void addOrderStatementByPopoIds(String orderStatementName, Long[] ids);
 }

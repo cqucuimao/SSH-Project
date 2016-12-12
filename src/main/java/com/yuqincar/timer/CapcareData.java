f2b1e7545f93d5e62d3f9309a7b40c851c0a076e
cuimao <cuimao@138713d6-2722-b04c-9809-0d1aa3304408>
2016-12-12 01:58:04 +0000

    git-svn-id: svn://101.201.31.147/yuqin/Web@888 138713d6-2722-b04c-9809-0d1aa3304408

--- src/main/java/com/yuqincar/timer/CapcareData.java
+++ src/main/java/com/yuqincar/timer/CapcareData.java
@@ -3,18 +3,23 @@ package com.yuqincar.timer;
 import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.yuqincar.service.monitor.CapcareMessageService;
 import com.yuqincar.utils.CapcareMessageUtils;
 
 @Component
 public class CapcareData {
+	@Autowired
+	private CapcareMessageService capcareMessageService;
 	
 	@Scheduled(cron = "0/10 * * * * ? ") 
 	@Transactional
 	public void update(){
-		CapcareMessageUtils.getCapcareMessage();
+		capcareMessageService.getCapcareMessagePerTenSecondFromCapcare();
+		//CapcareMessageUtils.getCapcareMessage();
 	}
 }

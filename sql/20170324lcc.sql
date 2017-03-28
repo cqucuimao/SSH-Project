0238dd9293db282ef1dbc5f99bc14e32b2744715
liucongcong <PRlab02@DESKTOP-7E2GRT4>
2017-03-28 16:32:04 +0800

    修改车辆默认司机的sql语句改正

--- sql/20170324lcc.sql
+++ sql/20170324lcc.sql
@@ -1,3 +1,3 @@
 ﻿insert into privilege(id,name,url) values(474,'修改车辆默认司机','/car_editDefaultDriver');
 insert into role_privilege(roles_id,privileges_id) values(1,474);
-insert into privilege(id,name,url,parent_id) values(475,'','/car_editDefaultDriverUI',471);
+insert into privilege(id,name,url,parent_id) values(475,'','/car_editDefaultDriverUI',474);

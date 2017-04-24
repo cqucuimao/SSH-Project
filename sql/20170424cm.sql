a017ed1bdee88ee90e999a1b305bd3fbc95e7b21
cuimao <cqucuimao@126.com>
2017-04-24 21:44:01 +0800

    sql提交

---
+++ sql/20170424cm.sql
@@ -0,0 +1,3 @@
+insert into privilege(id,name,url,parent_id) values(476,'','/realtimeChart_exportUI',260);
+insert into role_privilege(roles_id,privileges_id) values(1,476);
+insert into privilege(id,name,url,parent_id) values(477,'','/realtimeChart_export',476);
